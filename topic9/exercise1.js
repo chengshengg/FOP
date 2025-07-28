0df8d53e6540b26d78dd259cd44dd793d31af683
chengshengg <kongchengsheng@gmail.com>
2025-07-28 12:20:27 +0800

    a

--- topic9/exercise1.js
+++ topic9/exercise1.js
@@ -15,15 +15,40 @@ let r4 = {
     width: 5
 }
 let rectArray = [r1, r2, r3, r4]
-console.log(rectArray)
 
-for(i=0;i<rectArray.length;i++){
-    console.log(`r${i+1} ${rectArray[i].length} length ${rectArray[i].width} width`)
-}
-
-function calculateRectangleArea(arr){
+function getBiggestlength(arr){
+    biggest = 0
     for(i=0;i<arr.length;i++){
-        console.log(arr[i].length * arr[i].width)
+        if(arr[i].length > biggest){
+            biggest = arr[i].length
+        }
     }
+    return biggest
 }
-calculateRectangleArea(rectArray)
+
+function getBiggest(arr){
+    biggestLength = 0
+    biggestObj = null
+    for(i=0;i<arr.length;i++){
+        if(arr[i].length > biggestLength){
+            biggestLength = arr[i].length
+            biggestObj = arr[i]
+        }
+    }
+    return biggestLength, biggestObj
+}
+
+console.log(getBiggestlength(rectArray))
+console.log(getBiggest(rectArray))
+// console.log(rectArray)
+
+// for(i=0;i<rectArray.length;i++){
+//     console.log(`r${i+1} ${rectArray[i].length} length ${rectArray[i].width} width`)
+// }
+
+// function calculateRectangleArea(arr){
+//     for(i=0;i<arr.length;i++){
+//         console.log(arr[i].length * arr[i].width)
+//     }
+// }
+// calculateRectangleArea(rectArray)
